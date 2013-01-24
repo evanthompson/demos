f6147f0195f4d2f31a700e61b09892f06cc27e99
evanthompson <ethompson.cs@gmail.com>
2013-01-24 14:07:14 -0500

    removal of files

--- CollisionDemo/src/CollisionPack/InanimateObject.java
+++
@@ -1,9 +0,0 @@
-package CollisionPack;
-
-// Inanimate objects don't do much
-public class InanimateObject extends GameObject {
-
-	public InanimateObject(int x, int y, int type) {
-		super(x, y, type);
-	}
-}
